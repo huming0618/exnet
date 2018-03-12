eefb18b6f9f5c006b7b8d8248658e32b3306db66
Peter Hu <yesky93@sina.com>
2018-03-12 13:33:56 +0800

    try record data

--- ExWSClient.cs
+++ ExWSClient.cs
@@ -3,14 +3,33 @@ using System.Net.WebSockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace exnet
 {
     class ExWSClient
     {
         readonly String PATH = "wss://real.okex.com:10441/websocket";
+        readonly String[] WATCH_LIST;
+
         public ExWSClient(){
-            
+            WATCH_LIST = new String[]{
+
+                "{'event':'addChannel','channel':'ok_sub_spot_dadi_usdt_ticker'}",
+                "{'event':'addChannel','channel':'ok_sub_spot_dadi_usdt_depth'}",
+                // "{'event':'addChannel','channel':'ok_sub_spot_btc_usdt_depth_Y'}",
+                "{'event':'addChannel','channel':'ok_sub_spot_dadi_usdt_deals'}",
+
+                "{'event':'addChannel','channel':'ok_sub_spot_dadi_btc_ticker'}",
+                "{'event':'addChannel','channel':'ok_sub_spot_dadi_btc_depth'}",
+                // "{'event':'addChannel','channel':'ok_sub_spot_btc_usdt_depth_Y'}",
+                "{'event':'addChannel','channel':'ok_sub_spot_dadi_btc_deals'}",
+
+                "{'event':'addChannel','channel':'ok_sub_spot_dadi_eth_ticker'}",
+                "{'event':'addChannel','channel':'ok_sub_spot_dadi_eth_depth'}",
+                // "{'event':'addChannel','channel':'ok_sub_spot_btc_usdt_depth_Y'}",
+                "{'event':'addChannel','channel':'ok_sub_spot_dadi_eth_deals'}"
+            };
         }
 
         public void Init(){
@@ -23,17 +42,29 @@ namespace exnet
                 await ws.ConnectAsync(pathUri, CancellationToken.None);
                 if (ws.State == WebSocketState.Open){
                     Console.WriteLine("Connected to Server");
-                    var msg = "{'event':'addChannel','channel':'ok_sub_spot_btm_usdt_depth'}";
-                    ArraySegment<byte> bytesToSend = new ArraySegment<byte>(Encoding.UTF8.GetBytes(msg));
-                    await ws.SendAsync(bytesToSend, WebSocketMessageType.Text, true, CancellationToken.None);
-                
+
+                    foreach (var msg in WATCH_LIST)
+                    {
+                        ArraySegment<byte> bytesToSend = new ArraySegment<byte>(Encoding.UTF8.GetBytes(msg));
+                        await ws.SendAsync(bytesToSend, WebSocketMessageType.Text, true, CancellationToken.None);
+                    }
+
                     while(true){
                         Console.WriteLine("......");
                         Console.WriteLine(DateTime.Now.ToString("h:mm:ss tt"));
                         Console.WriteLine("Receiving ....");
                         ArraySegment<byte> bytesReceived = new ArraySegment<byte>(new byte[1024]);
                         WebSocketReceiveResult result = await ws.ReceiveAsync(bytesReceived, CancellationToken.None);
-                        Console.WriteLine(Encoding.UTF8.GetString(bytesReceived.Array, 0, result.Count));
+                        Console.WriteLine();
+
+                        var response = Encoding.UTF8.GetString(bytesReceived.Array, 0, result.Count);
+
+                        using (StreamWriter file = 
+                            new StreamWriter(@"/tmp/ex.log", true))
+                        {
+                            file.WriteLine(response);
+                        }
+                        Console.WriteLine(response);
                     }
                     
                 }
